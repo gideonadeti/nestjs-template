7ccf57b317d7c0bd9bf821b2d278bd7ef99f7c7f
Gideon Adeti <gideonadeti0@gmail.com>
2026-05-27 19:11:40 +0000

    feat: implement public route decorator and enhance ClerkAuthGuard
    - Introduced a new Public decorator to mark routes as public.
    - Updated ClerkAuthGuard to allow access to public routes without authentication.

--- src/clerk-auth/clerk-auth.guard.ts
+++ src/clerk-auth/clerk-auth.guard.ts
@@ -1,12 +1,24 @@
 import { Request } from 'express';
 import { getAuth } from '@clerk/express';
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+import { IS_PUBLIC_KEY } from 'src/public/public.decorator';
 
 @Injectable()
 export class ClerkAuthGuard implements CanActivate {
-  constructor() {}
+  constructor(private readonly reflector: Reflector) {}
 
   canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
     const request = context.switchToHttp().getRequest<Request>();
     const { userId } = getAuth(request);
 
