4a5f88bce681845d2995a11f617aacc707e95b96
Gideon Adeti <gideonadeti0@gmail.com>
2026-05-27 13:59:04 +0000

    feat: add @nestjs/config for environment configuration
    - Added @nestjs/config as a dependency in package.json.
    - Configured ConfigModule in app.module.ts to load environment variables from .env.local and .env files globally.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.local', '.env'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
