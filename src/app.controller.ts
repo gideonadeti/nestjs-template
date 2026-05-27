5d1d52a12da5915af85111cd381d62dc92153278
Gideon Adeti <gideonadeti0@gmail.com>
2026-05-27 20:55:28 +0000

    feat: add Public decorator to AppController for public route access
    - Introduced the Public decorator to mark the getHello route as accessible without authentication.
    - Updated AppController to utilize the new Public decorator, enhancing route visibility.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,12 +2,14 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 
 import { AppService } from './app.service';
 import { ClerkAuthGuard } from './clerk-auth/clerk-auth.guard';
+import { Public } from './public/public.decorator';
 
 @UseGuards(ClerkAuthGuard)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
   getHello(): string {
     return this.appService.getHello();
