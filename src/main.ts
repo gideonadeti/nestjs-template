022438dea32ca21a53aeeaf60cd6c5363c08d903
Gideon Adeti <gideonadeti0@gmail.com>
2026-05-27 12:47:18 +0000

    feat: integrate Swagger for API documentation
    - Added @nestjs/swagger as a dependency in package.json.
    - Configured SwaggerModule in main.ts to set up API documentation at '/api/documentation'.
    - Updated nest-cli.json to include Swagger plugin options for comment introspection.
    - Adjusted pnpm-workspace.yaml to allow builds for '@scarf/scarf'.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Template')
+    .setDescription('API documentation for NestJS Template')
+    .setVersion('1.0.0')
+    .build();
+
+  const options = {
+    operationIdFactory: (_controllerKey: string, methodKey: string) =>
+      methodKey,
+  };
+
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, config, options);
+
+  SwaggerModule.setup('api/documentation', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
